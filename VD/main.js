fe2bc46029f8bac257f180d9025d25652aeacbe4
NguyenDuyHieu987 <vaicut6941@gmail.com>
2022-04-20 22:13:45 +0700

    add

---
+++ VD/main.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector("button")
+
+function random_sRGB() {
+
+}
+
+btn.addEventListener("click", function()) {
+    let color = random_sRGB();
+    document.body.style.background = color;
+    let color2 = random_sRGB();
+    h1.style.color = color2;
+}
